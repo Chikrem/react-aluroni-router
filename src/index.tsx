4be2c843c3ffcc5776541a5c0b97037220c4d787
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-03-10 09:03:59 -0300

    Criando Recomendações de Pratos

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Cardapio />
-  </React.StrictMode>,
+  <Inicio />,
   document.getElementById('root')
 );
