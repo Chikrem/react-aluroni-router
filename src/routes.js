7de97e8bb1438908fb3652ae6b2083bb0609dd8f
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-03-10 10:31:30 -0300

    Criando Pagina Padrão e renderizando com Outlet

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,21 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';import  Inicio  from 'pages/Inicio';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
 import Menu from './common/menu/index';
+import PaginaPadrao from './common/paginaPadrao';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/Cardapio" element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element ={<PaginaPadrao/>}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }
