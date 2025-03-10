f0cc460ad22e8c38a135b16756672e36aed972a2
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-03-10 09:25:12 -0300

    Usando Router Dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
-import Inicio from 'pages/Inicio';
+import AppRouter from './routes';
 
 ReactDOM.render(
-  <Inicio />,
+  AppRouter(),
   document.getElementById('root')
 );
