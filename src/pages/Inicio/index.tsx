2e653041a47c662efda65eddc5cab7877db75cbe
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-03-10 09:45:44 -0300

    Reaproveitando Menu com react-router

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,4 +1,5 @@
-import cardapio from '../../data/cardapio.json'; 
+import Menu from 'common/menu';
+import cardapio from '../../data/cardapio.json';
 import styles from './Inicio.module.scss';
 
 function Inicio() {
@@ -8,19 +9,21 @@ function Inicio() {
     .splice(0, 3);
 
   return (
-    <section>
-      <h3 className={styles.titulo}>Recomendações da Cozinha</h3>
-      <div className={styles.recomendados}>
-        {pratosRecomendados.map((prato) => (
-          <div key={prato.id} className={styles.recomendado}>
-            <div className={styles.recomendado__imagem}>
-              <img src={prato.photo} alt={prato.title} />
+    <>
+      <section>
+        <h3 className={styles.titulo}>Recomendações da Cozinha</h3>
+        <div className={styles.recomendados}>
+          {pratosRecomendados.map((prato) => (
+            <div key={prato.id} className={styles.recomendado}>
+              <div className={styles.recomendado__imagem}>
+                <img src={prato.photo} alt={prato.title} />
+              </div>
+              <button className={styles.recomendado__botao}>Ver Mais</button>
             </div>
-            <button className={styles.recomendado__botao}>Ver Mais</button>
-          </div>
-        ))}
-      </div>
-    </section>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';import  Inicio  from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
+import Menu from './common/menu/index';
 
 export default function AppRouter() {
   return (
     <Router>
+      <Menu />
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/Cardapio" element={<Cardapio />} />
