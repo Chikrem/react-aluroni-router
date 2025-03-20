45ae4bca1d6180c2127b185e630298139d81ef7e
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-03-20 17:11:04 -0300

    Aplicando ReactLazy nos componentes

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,36 +1,44 @@
 import styles from './Prato.module.scss';
-import { useParams, useNavigate } from 'react-router-dom';
-import cardapio from '../../data/cardapio.json';
-import TagsPrato from 'common/TagsPrato';
-import NotFound from 'pages/NotFound';
-import PaginaPadrao from 'common/paginaPadrao';
+import { useParams, useNavigate, Routes, Route } from 'react-router-dom';
+import cardapio from 'data/cardapio.json';
+import { lazy } from 'react';
+
+const TagsPrato = lazy(() => import('common/TagsPrato'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const PaginaPadrao = lazy(() => import('common/paginaPadrao'));
 
 export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = cardapio.find(item => item.id === Number(id));
-  if(!prato) {
+  if (!prato) {
     return <NotFound />;
   }
   return (
-    <PaginaPadrao>
-      <button className={styles.voltar} onClick={() => navigate(-1)}>
-        {'< Voltar'}
-      </button>
-      <div className={styles.container}>
-        <h1 className={styles.titulo}>
-          {prato.title}
-        </h1>
-        <div className={styles.imagem}>
-          <img src={prato.photo} alt={prato.title} />
-        </div>
-        <div className={styles.conteudo}>
-          <p className={styles.conteudo__descricao}>
-            {prato.description}
-          </p>
-        </div>
-        <TagsPrato {...prato} />
-      </div>
-    </PaginaPadrao>
+    <Routes>
+      <Route path="*" element={<PaginaPadrao />}>
+        <Route index element={
+          <>
+            <button className={styles.voltar} onClick={() => navigate(-1)}>
+              {'< Voltar'}
+            </button>
+            <section className={styles.container}>
+              <h1 className={styles.titulo}>
+                {prato.title}
+              </h1>
+              <div className={styles.imagem}>
+                <img src={prato.photo} alt={prato.title} />
+              </div>
+              <div className={styles.conteudo}>
+                <p className={styles.conteudo__descricao}>
+                  {prato.description}
+                </p>
+                <TagsPrato {...prato} />
+              </div>
+            </section>
+          </>
+        } />
+      </Route>
+    </Routes>
   );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,21 +1,21 @@
 import Footer from 'common/Footer';
 import Menu from 'common/menu';
-import PaginaPadrao from 'common/paginaPadrao';
-import Inicio from 'pages/Inicio';
-import NotFound from 'pages/NotFound';
-import Prato from 'pages/Prato';
-import Sobre from 'pages/Sobre';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const Cardapio = lazy(() => import('pages/Cardapio'));
+const PaginaPadrao = lazy(() => import('common/paginaPadrao'));
+const Inicio = lazy(() => import('pages/Inicio'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const Prato = lazy(() => import('pages/Prato'));
+const Sobre = lazy(() => import('pages/Sobre'));
 
 export default function AppRouter() {
   return (
     <main className='container'>
       <Router>
+        <Menu />
         <Suspense fallback={<p> Carregando... </p>}>
-          <Menu />
           <Routes>
             <Route path='/' element={<PaginaPadrao />}>
               <Route index element={<Inicio />} />
@@ -25,8 +25,8 @@ export default function AppRouter() {
             <Route path='prato/:id' element={<Prato />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
-          <Footer />
         </Suspense>
+        <Footer />
       </Router>
     </main>
   );
